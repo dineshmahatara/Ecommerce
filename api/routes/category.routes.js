605108f9e59a0c8348a72a27446048613b6a4b89
Sandesh Bhattarai <sandesh.bhattarai@kotuko.it>
2022-12-16 06:45:12 +0545

    Category CRUD

--- api/routes/category.routes.js
+++ api/routes/category.routes.js
@@ -1,18 +1,18 @@
 const auth = require("../app/middleware/auth.middleware");
+const { isAdmin } = require("../app/middleware/rbac.middleware");
+const uploader = require("../app/middleware/uploader.middleware");
+
+const CategoryController = require("../app/controller/category.controller");
+const category_ctrl = new CategoryController();
 
 const router = require("express").Router();
 
 router.route("/")
-    .get((req, res, next) =>{
+    .get(category_ctrl.getCategories)
+    .post(auth, isAdmin, uploader.single('image'), category_ctrl.categoryStore)
 
-    })
-    .post(auth, (req, res, next) => {
-        // category create
-        res.json({
-            result: req.auth_user,
-            status: true, 
-            msg: "Category..."
-        })
-    })
-
+router.route("/:id")
+    .get(category_ctrl.getCategoryById)
+    .delete(auth, isAdmin, category_ctrl.deleteById)
+    .put(auth, isAdmin,uploader.single("image"), category_ctrl.categoryUpdate)
 module.exports = router;
